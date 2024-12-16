511f565170895356dcb0993ad51b8634f67f9c25
cyril-hhf <cyrilhhf@gmail.com>
2024-12-16 16:43:30 +0800

    updated rollDice, held dice wont get randomized by triggering the rollDice function

--- Tenzies/App.jsx
+++ Tenzies/App.jsx
@@ -17,7 +17,11 @@ export default function App() {
     }
 
     function rollDice() {
-        setDice(generateAllNewDice())
+        setDice(oldDice => oldDice.map(die =>
+            die.isHeld ?
+                die :
+                { ...die, value: Math.ceil(Math.random() * 6)}
+        ))
     }
 
     const hold = function hold(id) {
