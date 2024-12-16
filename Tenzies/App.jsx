9d4caabe919fbf892a3cafc2420a6d9226df3ca1
cyril-hhf <cyrilhhf@gmail.com>
2024-12-16 22:23:59 +0800

    added confetti when user win the game

--- Tenzies/App.jsx
+++ Tenzies/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
 
     const [dice, setDice] = useState(generateAllNewDice())
+    const gameWon = (dice.every(die => die.isHeld) &&
+                     dice.every(die => die.value === dice[0].value))
 
-    if (dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value)
-    ) {
-        console.log("won")
-    }
+    const buttonRef = useRef(null)
 
+    // useEffect(() => {
+
+    // }, [gameWon])
 
     function generateAllNewDice() {
         return new Array(10)
@@ -45,12 +47,15 @@ export default function App() {
 
     return (
         <main>
+            {gameWon && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
