1c755102f46d2e478d63abb09323e28a923d76d9
cyril-hhf <cyrilhhf@gmail.com>
2024-12-16 15:45:08 +0800

    App.jsx: added function to create an array of 10 random number. TODO: 1. show on dice. 2. create object so that it can hold a property indicating whether it is held or not.

--- Tenzies/App.jsx
+++ Tenzies/App.jsx
@@ -1,3 +1,26 @@
+import Die from "./Die"
+
 export default function App() {
-    return <main></main>
+
+    function generateAllDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    console.log(generateAllDice())
+
+    return <main>
+                <div className="dice-container">
+                    <Die value={1} />
+                    <Die value={2} />
+                    <Die value={3} />
+                    <Die value={4} />
+                    <Die value={5} />
+                    <Die value={6} />
+                    <Die value={1} />
+                    <Die value={1} />
+                    <Die value={1} />
+                    <Die value={1} />
+                </div>
+            </main>
 }
